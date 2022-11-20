ec940ffe0d197e77cae908a775bcb81972a6cc8e
NuriaSan <117538455+NuriaSan@users.noreply.github.com>
2022-11-20 21:43:34 +0100

    Add files via upload
    Subida de Archivos

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main (String[] args) {
+
+        Pfuturo resultado = new Pfuturo();
+
+        String estudiante= "Nuria Sanchez";
+
+        System.out.println("Estudiante " + args[0] + " : ");
+
+        resultado.veredicto("6" , "35" );
+    }
+}
---
+++ Pfuturo.java
@@ -0,0 +1,14 @@
+public class Pfuturo {
+    private String valor1;
+    private String valor2;
+
+    public Pfuturo (){}
+    static String veredicto(String valor1, String valor2){
+        if (valor1.equals(valor2)) {
+                System.out.println ("Bien. Pero una hora de estudio para cada módulo puede ser insuficiente.");
+        } else if (Integer.parseInt(valor1)>Integer.parseInt(valor2)) {System.out.println ("Poco tiempo de estudio. Debes dedicar más tiempo.");
+        }else System.out.println("Ideal. Trabajas los contenidos en casa.");
+        return"";
+
+    }
+}
